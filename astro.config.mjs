356479ef8e1a7e2361901008ef8694ee16fc3426
40ftslinky <40289122+40ftslinky@users.noreply.github.com>
2023-11-30 16:57:49 +1100

    1 file modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site: 'https://40ftslinky.github.io',
+  base: '/astro-portfolio',
 });
